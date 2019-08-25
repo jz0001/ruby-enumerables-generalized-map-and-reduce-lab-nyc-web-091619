c4457499b036cb609971d38878502ef36d235e5a
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-25 23:59:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,1 @@
 # Your Code Here
-def make_sandwich(element1, element2)
-  base = "A #{element1} and #{element2}"
-  yield(base)
-end
- 
-make_sandwich("gator", "gumbo") do |innards|
-  innards
-end
