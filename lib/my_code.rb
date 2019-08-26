004c21c8556f3306019ec9f394ec9996be9f7f8a
Jinming Zhang <jinming.zhang.92@gmail.com>
2019-08-26 00:30:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,10 +10,12 @@ end
 def reduce(array,starting_point=nil)
   if starting_point
     total=starting_point
+    index=0
   else
     total=array[0]
+    index=1
   end
-  array.length.times {|index|
-    total=yield(total,array[index])}
+  while index<array.length do 
+    total=yield(total,array[index])
     total
   end
